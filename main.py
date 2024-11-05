1b974d4b3fc09e134d63e091ab4ffc77c7293872
Luan Tran <luancaovotran@gmail.com>
2024-11-05 15:25:38 +0100

    ?

--- main.py
+++ main.py
@@ -11,19 +11,21 @@ board = Arduino('/dev/ttyACM0')  # Adjust for your operating system
 # led_pin = board.get_pin('d:6:o')
 
 # Define the digital pin 6 as pwm
-pwm_pin = board.get_pin('d:6:o') 
+pwm_pin = board.get_pin('d:6:p') 
 
 
 while True:
     # # Simple blinking code
-    pwm_pin.write(True)  
-    time.sleep(0.1)
-    pwm_pin.write(False) 
-    time.sleep(0.1)
+    # led_pin.write(True)  
+    # time.sleep(0.1)
+    # led_pin.write(False) 
+    # time.sleep(0.1)
 
-    # for i in range(0, 256):  # i ranges from 0 to 100
-    #     dt = i/255
-    #     print("DT: ", dt)
-    #     pwm_pin.write(False)
-    #     print("Iteration: ", i)
-    #     time.sleep(0.5)  # small delay
+    for i in range(0, 256):  # i ranges from 0 to 100
+        # dt = i/255
+        # print("DT: ", dt)
+        pwm_pin.write(0.5)
+        # print("Iteration: ", i)
+        time.sleep(0.5)  # small delay
+        pwm_pin.write(0.9)
+        time.sleep(0.5)
