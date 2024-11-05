d8da98b2830f9b3e3f7e822bb09217f5c74c526e
Luan Tran <luancaovotran@gmail.com>
2024-11-05 15:02:04 +0100

    PWM test

--- main.py
+++ main.py
@@ -7,28 +7,21 @@ from pyfirmata import Arduino, util
 # Set up the connection to the Arduino board
 board = Arduino('/dev/ttyACM0')  # Adjust for your operating system
 
-# Define the pin for 0
-led_pin = board.get_pin('d:6:o') 
-# board.digital[6].mode = 1
-# pwm = board.get_pin('d:6:p') 
-# board.digital[6].mode = 3
+# Define the digital pin 6 as output
+# led_pin = board.get_pin('d:6:o')
+
+# Define the digital pin 6 as pwm
+pwm_pin = board.get_pin('d:6:p') 
 
 
 while True:
-    # Simple blinking code
-    led_pin.write(True)  
-    time.sleep(0.1)
-    led_pin.write(False) 
-    time.sleep(0.1)
+    # # Simple blinking code
+    # led_pin.write(True)  
+    # time.sleep(0.1)
+    # led_pin.write(False) 
+    # time.sleep(0.1)
 
-    # for i in range(0, 101):  # i ranges from 0 to 100
-    #     duty_cycle = i / 100  # convert to a range between 0 and 1
-    #     board.digital[6].write(duty_cycle)  # Write PWM duty cycle to pin
-    #     time.sleep(0.05)  # small delay
-
-    # # Gradually decrease the PWM signal
-    # for i in range(100, -1, -1):  # i ranges from 100 down to 0
-    #     duty_cycle = i / 100
-    #     board.digital[6].write(duty_cycle)
-    #     time.sleep(0.05)
+    for i in range(0, 256):  # i ranges from 0 to 100
+        pwm_pin.write(i)
+        time.sleep(0.3)  # small delay
 
