a0b1b352309ff3522ec101a64445921b341f0fcd
Luan Tran <luancaovotran@gmail.com>
2024-11-05 16:06:26 +0100

    pwm

--- main.py
+++ main.py
@@ -7,26 +7,27 @@ from pyfirmata import Arduino, util
 # Set up the connection to the Arduino board
 board = Arduino('/dev/ttyACM0')  # Adjust for your operating system
 
-# Define the digital pin 6 as output
-led_pin = board.get_pin('d:3:p')
+# Define the digital pin 3 as output
+# led_pin = board.get_pin('d:3:0')
 
 # Define the digital pin 3 as pwm
-# pwm_pin = board.get_pin('d:3:p') 
+pwm_pin = board.get_pin('d:3:p') 
 
 
 while True:
     # # Simple blinking code
-    led_pin.write(True)  
-    time.sleep(0.1)
-    led_pin.write(False) 
-    time.sleep(0.1)
-    print(board.digital[3].mode)
+    # led_pin.write(True)  
+    # time.sleep(0.1)
+    # led_pin.write(False) 
+    # time.sleep(0.1)
+
+    pwm_pin.write(0.1)
 
     # for i in range(0, 256):  # i ranges from 0 to 100
-    #     # dt = i/255
-    #     # print("DT: ", dt)
-    #     pwm_pin.write(0.5)
-    #     print("Iteration: ", i)
-    #     time.sleep(0.5)  # small delay
-    #     pwm_pin.write(0.9)
-    #     time.sleep(0.5)
+        # # dt = i/255
+        # # print("DT: ", dt)
+        # pwm_pin.write(0.5)
+        # print("Iteration: ", i)
+        # time.sleep(0.5)  # small delay
+        # pwm_pin.write(0.9)
+        # time.sleep(0.5)
