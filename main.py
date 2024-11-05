493aef45038cc5de4889d6b255d948748acc31e6
Luan Tran <luancaovotran@gmail.com>
2024-11-05 14:40:42 +0100

    Small change

--- main.py
+++ main.py
@@ -14,11 +14,8 @@ board.digital[6].mode = 1
 
 # Run for 10 seconds, changing the brightness
 while True:
-# for value in range(0, 256, 5):  # Gradually increase brightness
     led_pin.write(True)  # Convert to 0.0 - 1.0 range
     time.sleep(0.1)  # Wait for 100 milliseconds
-
-# for value in range(255, -1, -5):  # Gradually decrease brightness
     led_pin.write(False)  # Convert to 0.0 - 1.0 range
-    time.sleep(0.1)  # Wait for 100 milliseconds
+    # time.sleep(0.1)  # Wait for 100 milliseconds
 
