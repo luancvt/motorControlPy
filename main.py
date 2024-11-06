103a267deccb4d7aa17295c5e1a75fa78fa70109
Luan Tran <luancaovotran@gmail.com>
2024-11-06 17:27:26 +0100

    qwe

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ board = Arduino('/dev/ttyACM0')  # Adjust for your operating system
 # led_pin = board.get_pin('d:6:o')
 
 # Define the digital pin 6 as pwm
-pwm_pin = board.get_pin('d:10:p')
+LED = board.digital[9]
+LED.mode = 3
 
 print("CODE INIT")
 
@@ -25,6 +26,6 @@ while True:
     for i in range(0, 256):  # i ranges from 0 to 100
         dt = i/255
         print("DT: ", dt)
-        pwm_pin.write(dt)
+        LED.write(dt)
         print("Iteration: ", i)
         time.sleep(0.1)  # small delay
