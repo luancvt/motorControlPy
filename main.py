15a85bd751aafabf1013565344b97c4ade804508
Luan Tran <luancaovotran@gmail.com>
2024-11-07 12:52:09 +0100

    Test

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ from pyfirmata import Arduino, util
 board = Arduino('/dev/ttyACM0')  # Adjust for your operating system
 
 # Define the digital pin 6 as output
-# led_pin = board.get_pin('d:6:o')
+led_pin = board.get_pin('d:6:o')
 
 # Define the digital pin 6 as pwm
-LED = board.digital[9]
-LED.mode = 3
+# LED = board.digital[9]
+# LED.mode = 3
 
 print("CODE INIT")
 
 while True:
-    # Simple blinking code
-    # led_pin.write(True)  
-    # time.sleep(0.1)
-    # led_pin.write(False) 
-    # time.sleep(0.1)
+    #Simple blinking code
+    led_pin.write(True)  
+    time.sleep(0.1)
+    led_pin.write(False) 
+    time.sleep(0.1)
 
-    for i in range(0, 256):  # i ranges from 0 to 100
-        dt = i/255
-        print("DT: ", dt)
-        LED.write(1)
-        print("Iteration: ", i)
-        time.sleep(0.1)  # small delay
+    # for i in range(0, 256):  # i ranges from 0 to 100
+    #     dt = i/255
+    #     print("DT: ", dt)
+    #     LED.write(1)
+    #     print("Iteration: ", i)
+    #     time.sleep(0.1)  # small delay
